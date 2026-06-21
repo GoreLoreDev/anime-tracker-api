5a774f39750ec4bf69d4e696197ede558857af6c
GoreLoreDev <anuranpaul19@gmail.com>
2026-06-21 18:43:26 +0530

    add the postgre sql and let the database generate the AnimeID automatically also make AnimeRepository.java

--- src/main/java/com/anuran/anime_tracker_api/model/Anime.java
+++ src/main/java/com/anuran/anime_tracker_api/model/Anime.java
@@ -1,8 +1,20 @@
+
+
 package com.anuran.anime_tracker_api.model;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
 public class Anime {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
+
 
     public Anime(Long id, String title) {
         this.id = id;
@@ -21,4 +33,12 @@ public class Anime {
     public void setTitle(String title){
         this.title=title;
     }
+
+
+    //Hibernate/JPA needs to be able to create objects itself.
+    // if it needs to do something likeAnime anime = new Anime(); then it needs this no arg constructor
+    public Anime() {
+
+    }
 }
+
